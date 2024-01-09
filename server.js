35715b9cd614bb1155235380a8e5a7d8b27d96f9
sabata911 <151187251+sabata911@users.noreply.github.com>
2024-01-09 18:47:32 +0300

    Update server.js

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ const mysql = require('mysql2');
 // create the connection to database
 const connection = mysql.createConnection({
   host: 'db-service',
+  port: '3306',
   user: 'root',
   database: 'test',
   password: 'Admin123!'
